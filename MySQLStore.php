9464ed743cfd69e2bcc84da8d1a16a4d734ce42d
Eugene F. Barker <genebarker@gmail.com>
2020-02-21 14:11:11 +1400

    Complete open and close for the MySQLStore

--- MySQLStore.php
+++ MySQLStore.php
@@ -4,23 +4,28 @@ namespace madman\Password;
 
 class MySQLStore implements Datastore
 {
+    public $conn = null;
+
     public function openConnection($host, $username, $password, $db_name)
     {
         $link = mysql_connect($host, $username, $password);
         mysql_select_db($db_name);
-        return $link;
+        $this->conn = $link;
     }
 
     public function getConnection()
     {
+        return $this->conn;
     }
 
-    public function setConnection($conn)
+    public function setConnection($link)
     {
+        $this->conn = $link;
     }
 
     public function closeConnection()
     {
+        mysql_close($this->conn);
     }
 
     public function getVersion()
--- tests/MySQLStoreTest.php
+++ tests/MySQLStoreTest.php
@@ -20,22 +20,55 @@ class MySQLStoreTest extends TestCase
         $this->assertTrue($store instanceof Datastore);
     }
 
-    public function testOpenConnectionOpensOne()
+    public function testOpenConnectionOpens()
+    {
+        $store = $this->getDatastore();
+        $conn = $store->conn;
+
+        $sql = 'SELECT DATABASE();';
+        $result = mysql_query($sql, $conn);
+        $row = mysql_fetch_row($result);
+        $this->assertEquals(
+            $this->db_config['db_name'],
+            $row[0]
+        );
+    }
+
+    private function getDatastore()
     {
         $store = new MySQLStore();
-        $link = $store->openConnection(
+        $store->openConnection(
+            $this->db_config['host'],
+            $this->db_config['username'],
+            $this->db_config['password'],
+            $this->db_config['db_name']
+        );
+        return $store;
+    }
+
+    public function testCloseConnectionCloses()
+    {
+        $store = $this->getDatastore();
+        $conn = $store->conn;
+        $store->closeConnection();
+
+        $this->assertFalse(
+            is_resource($conn)
+            && get_resource_type($conn) === 'mysql link'
+        );
+    }
+
+    public function testSetConnectionSets()
+    {
+        $link = mysql_connect(
             $this->db_config['host'],
             $this->db_config['username'],
             $this->db_config['password'],
             $this->db_config['db_name']
         );
 
-        $sql = 'SELECT DATABASE();';
-        $result = mysql_query($sql, $link);
-        $row = mysql_fetch_row($result);
-        $this->assertEquals(
-            $this->db_config['db_name'],
-            $row[0]
-        );
+        $store = new MySQLStore();
+        $store->setConnection($link);
+        $this->assertEquals($link, $store->getConnection());
     }
 }
