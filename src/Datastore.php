6a397fd31cbd9ce758f4d1cbf55b7ee6d56206e4
Eugene F. Barker <genebarker@gmail.com>
2020-02-27 12:24:50 +1400

    Ignore errant PHPCS const warning
    This is a confirmed bug in PHPCS.

--- src/Datastore.php
+++ src/Datastore.php
@@ -4,9 +4,7 @@ namespace madman\Password;
 
 interface Datastore
 {
-    // phpcs:disable
     const UNKNOWN_USERNAME = 1001;
-    // phpcs:enable
 
     public function openConnection($host, $username, $password, $db_name);
     public function getConnection();
