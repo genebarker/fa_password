51c7c0edd95498c93592429cd1bb1a8fdc96c23e
Eugene F. Barker <genebarker@gmail.com>
2020-02-27 16:21:24 +1400

    Add User equals tests

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -6,6 +6,8 @@ use PHPUnit\Framework\TestCase;
 
 class UserTest extends TestCase
 {
+    const NOON_XMAS_2019 = '2019-12-25 12:00:00';
+
     public function testConstructsAsExpected()
     {
         $oid = 42;
@@ -18,5 +20,38 @@ class UserTest extends TestCase
         $this->assertEquals(false, $user->is_locked);
         $this->assertEquals(0, $user->ongoing_pw_fail_count);
         $this->assertEquals(null, $user->last_pw_fail_time);
+    }
+
+    public function testEqualsTrueWhenSame()
+    {
+        $user_one = $this->createTestUser();
+        $user_two = $this->createTestUser();
+        $this->assertEquals($user_one, $user_two);
+    }
+
+    public static function createTestUser()
+    {
+        $oid = 26;
+        $username = 'kobe';
+        $pw_hash = ( // password = 'bryant'
+            '$2y$10$dxUH1PYc7dQpjBf5l2jfxO9Ce5M97m11ZIpP5IsjvfhdWBcPhWg/u'
+        );
+        $user = new User($oid, $username);
+        $user->pw_hash = $pw_hash;
+        $user->needs_pw_change = true;
+        $user->is_locked = true;
+        $user->ongoing_pw_fail_count = 20;
+        $user->last_pw_fail_time = MySQLStore::convertToPHPDate(
+            self::NOON_XMAS_2019
+        );
+        return $user;
+    }
+
+    public function testEqualsFalseWhenAnAttrDifferent()
+    {
+        $user_one = $this->createTestUser();
+        $user_two = $this->createTestUser();
+        $user_two->is_locked = false;
+        $this->assertNotEquals($user_one, $user_two);
     }
 }
