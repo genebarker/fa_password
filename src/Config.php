b4e5e8b89fe0600f56b843f8455017b4ac11c4e7
Eugene F. Barker <genebarker@gmail.com>
2020-03-11 10:52:51 +1400

    Increase default minimum password strength

--- src/Config.php
+++ src/Config.php
@@ -6,7 +6,7 @@ class Config
 {
     const DEFAULT_LOGIN_FAIL_THRESHOLD_COUNT = 3;
     const DEFAULT_LOGIN_FAIL_LOCK_MINUTES = 15;
-    const DEFAULT_MINIMUM_PASSWORD_STRENGTH = 2;
+    const DEFAULT_MINIMUM_PASSWORD_STRENGTH = 3;
 
     public $login_fail_threshold_count;
     public $login_fail_lock_minutes;
