c5515a153d706fe1f6d883080671a5c80352e3b2
Eugene F. Barker <genebarker@gmail.com>
2020-03-13 09:54:52 +1400

    Refactor Password Security Setup for readability

--- password_setup.php
+++ password_setup.php
@@ -1,17 +1,18 @@
-<?php
+<?php // phpcs:ignore
 
-require_once(__DIR__ . '/autoload.php');
 use madman\Password\Config;
 use madman\Password\MySQLStore;
 
 $page_security = 'SA_PASSWORDSETUP';
 $path_to_root = "../..";
+
+require_once(__DIR__ . '/autoload.php');
+require_once($path_to_root . "/includes/ui.inc");
+
+// Set page security
 require_once($path_to_root . "/includes/session.inc");
 add_access_extensions();
-
 page(_($help_context = "Password Security Setup"));
-
-require_once($path_to_root . "/includes/ui.inc");
 
 function ref_row_with_note($label, $value, $note = '')
 {
@@ -31,6 +32,42 @@ function save_password_config($store)
     $store->updateConfig($config);
 }
 
+function render_password_setup_form($pw_config)
+{
+    global $login_max_attempts, $login_delay;
+
+    start_form();
+    start_outer_table(TABLESTYLE2);
+    table_section(1);
+
+    $th = ['Setting', 'Value', 'Description'];
+    table_header($th);
+
+    ref_row_with_note(
+        'minimum_password_strength',
+        $pw_config->minimum_password_strength,
+        "Minimum zxcvbn password strength score (0-4): where 0 is too " .
+        "guessable and 4 is very unguessable."
+    );
+    ref_row_with_note(
+        'login_fail_threshold_count',
+        $pw_config->login_fail_threshold_count,
+        "Account locks after consecutive password failures exceed this. Keep " .
+        "this less than FA's \$login_max_attempts ($login_max_attempts)."
+    );
+    ref_row_with_note(
+        'login_fail_lock_minutes',
+        $pw_config->login_fail_lock_minutes,
+        "Login attempts are rejected until lock expires. Keep this value " .
+        "less than FA's \$login_delay ($login_delay)."
+    );
+
+    end_outer_table(1);
+    submit_center('set_pwe_config', _("Update"), true, '', 'default');
+    end_form(2);
+    end_page();
+}
+
 $store = new MySQLStore();
 $store->setConnection($db);
 
@@ -42,39 +79,4 @@ if (isset($_POST['set_pwe_config'])) {
 }
 
 $pw_config = $store->getConfig();
-
-start_form();
-
-start_outer_table(TABLESTYLE2);
-
-table_section(1);
-
-$th = ['Setting', 'Value', 'Description'];
-table_header($th);
-
-ref_row_with_note(
-    'minimum_password_strength',
-    $pw_config->minimum_password_strength,
-    "Minimum zxcvbn password strength score (0-4): where 0 is too " .
-    "guessable and 4 is very unguessable."
-);
-ref_row_with_note(
-    'login_fail_threshold_count',
-    $pw_config->login_fail_threshold_count,
-    "Account locks after consecutive password failures exceed this. Keep " .
-    "this less than FA's \$login_max_attempts ($login_max_attempts)."
-);
-ref_row_with_note(
-    'login_fail_lock_minutes',
-    $pw_config->login_fail_lock_minutes,
-    "Login attempts are rejected until lock expires. Keep this value " .
-    "less than FA's \$login_delay ($login_delay)."
-);
-
-end_outer_table(1);
-
-submit_center('set_pwe_config', _("Update"), true, '', 'default');
-
-end_form(2);
-
-end_page();
+render_password_setup_form($pw_config);
